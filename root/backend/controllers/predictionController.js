4338d9abdf5994e2292861d4ebae485a49f83fd5
Ivan Andika <ivanandika@u.nus.edu>
2020-11-10 10:40:38 +0800

    Controllers for overall prediction stats:

--- root/backend/controllers/predictionController.js
+++ root/backend/controllers/predictionController.js
@@ -58,3 +58,53 @@ exports.sync_delay_dancer = async function (req, res, next) {
                         ]) 
   res.status(200).send({syncDelay: avg_delay[0].avgDelay});
 }
+
+
+exports.move_accuracy_overall = async function(req, res, next) {
+  const dancerName = req.query.dancerName;
+  const dancesAccuracies = [["windows", 0.0], ["pushback", 0.0], ["elbowlock", 0.0], ["rocket", 0.0], ["hair", 0.0], 
+                            ["zigzag", 0.0], ["scarecrow", 0.0], ["shouldershrug", 0.0]]
+  let accuracies = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+  let correctCounts = [0,0,0,0,0,0,0,0];
+  let incorrectCounts = [0,0,0,0,0,0,0,0];
+  for (let i = 0; i < 8; i++) {
+    correctCounts[i] = await Prediction.countDocuments({isCorrect: true, action: dancesAccuracies[i][0]})
+    incorrectCounts[i] = await Prediction.countDocuments({isCorrect: false, action: dancesAccuracies[i][0]})
+    if (correctCounts[i] + incorrectCounts[i] !== 0) {
+      //console.log(dances[i], "Correct: ", correctCounts[i])
+      //console.log(dances[i], "Incorrect: ", incorrectCounts[i])
+      dancesAccuracies[i][1] = parseInt((correctCounts[i] / (correctCounts[i] + incorrectCounts[i])) * 100) 
+      accuracies[i] = parseInt((correctCounts[i] / (correctCounts[i] + incorrectCounts[i])) * 100) 
+    }
+  }
+  console.log(dancesAccuracies);
+  res.status(200).send({accuracies: accuracies,
+                        ascDances: dancesAccuracies.sort((a, b) => a[1] - b[1])});
+}
+
+exports.accuracy_dancer = async function (req, res, next) {
+  let accuracy = 0.0
+  correctCount = await Prediction.countDocuments({isCorrect: true});
+  incorrectCount = await Prediction.countDocuments({isCorrect: false});
+  if (correctCount + incorrectCount !== 0) {
+    accuracy = correctCount / (incorrectCount + correctCount);
+  }
+  console.log("accuracy", accuracy);
+  //cannot send number
+  res.status(200).send({accuracyDancer: accuracy});
+}
+
+// need to convert from string to float
+exports.sync_delay_overall = async function (req, res, next) {
+  let avg_delay = await Prediction.aggregate([
+                        {
+                          $group: {
+                            _id: null,
+                            avgDelay: { 
+                              $avg: "$syncdelay"
+                            }
+                          }
+                        }
+                        ]) 
+  res.status(200).send({syncDelay: avg_delay[0].avgDelay});
+}
---
+++ root/frontend/src/components/OverallStats/OverallStats.js
@@ -0,0 +1,171 @@
+import React, { useEffect , useState} from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+
+import UserCard from "../UserCard/UserCard"
+import UserDanceAccuracyBar from "../UserDanceAccuracyBar/UserDanceAccuracyBar"
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(1),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+  fixedUserStatsHeight : {
+    height: 900,
+  },
+  listPaper: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  title: {
+    margin: theme.spacing(4, 0, 2),
+  },
+}));
+
+const getDances = async (setBestMoves, setWorstMoves, dancerName) => {
+  try {
+    const url = new URL('http://localhost:4000/prediction/moveAccuracyDancer')
+    const params = {dancerName : dancerName}
+    url.search = new URLSearchParams(params).toString()
+    const response = await fetch(url)
+    const moveAccuraciesDancers = await response.json()
+    const ascDances = moveAccuraciesDancers.ascDances
+    setBestMoves([ascDances[7][0], ascDances[6][0], ascDances[5][0]])
+    setWorstMoves([ascDances[0][0], ascDances[1][0], ascDances[2][0]])
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+const getAccuracy = async (setAccuracy, dancerName) => {
+  try {
+    const url = new URL('http://localhost:4000/prediction/accuracyDancer')
+    const params = {dancerName : dancerName}
+    url.search = new URLSearchParams(params).toString()
+    const response = await fetch(url)
+    const resAccuracy = await response.json()
+    const accuracy = resAccuracy.accuracyDancer;
+    setAccuracy((accuracy * 100))
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+const getSyncDelay = async (setSyncDelay, dancerName) => {
+  try {
+    const url = new URL('http://localhost:4000/prediction/syncDelayDancer')
+    const params = {dancerName : dancerName}
+    url.search = new URLSearchParams(params).toString()
+    const response = await fetch(url)
+    const resSyncDelay = await response.json()
+    const syncDelay = resSyncDelay.syncDelay;
+    console.log("sync delay", syncDelay)
+    setSyncDelay(syncDelay);
+  } catch (err) {
+    console.error(err.message)
+  }
+}
+
+function OverallStats() {
+  const classes = useStyles();
+  const [bestMoves, setBestMoves] = useState(['','',''])
+  const [worstMoves, setWorstMoves] = useState(['','',''])
+  const [accuracy, setAccuracy] = useState(0);
+  const [syncDelay, setSyncDelay] = useState(0.0);
+  const userStatsHeight = clsx(classes.paper, classes.fixedUserStatsHeight)
+  //TODO: Make sure that it doesn't make too many requests (check double renders etc)
+  useEffect(() => {
+    getDances(setBestMoves, setWorstMoves, name)
+    getSyncDelay(setSyncDelay, name)
+    getAccuracy(setAccuracy, name)
+    const interval = setInterval(() => {
+      getDances(setBestMoves, setWorstMoves, name)
+    }, 10000)
+
+    return () => clearInterval(interval)
+  }, [])
+  return(
+    <Paper className={userStatsHeight}>
+      <Grid container spacing={4}>
+        <Grid item xs={4}>
+          <UserCard dancerName={name} />
+        </Grid>
+        <Grid item xs={7}>
+          <Grid container spacing={3}>
+            <Grid item xs={6}>
+              <Typography variant="h6" className={classes.title} gutterBottom>
+                Best Moves
+              </Typography>
+              <div className={classes.listPaper}>
+                <List>
+                    {bestMoves.map(move => (
+                      <div>
+                        <ListItem>
+                        <ListItemText
+                          primary={move}
+                          />
+                        </ListItem>
+                        <Divider />
+                      </div>
+                      
+                    ))}
+                </List>
+              </div>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant="h6" className={classes.title} gutterBottom>
+                Confusing Moves 
+              </Typography>
+              <div className={classes.listPaper}>
+                <List>
+                    {worstMoves.map(move => (
+                      <div>
+                        <ListItem>
+                        <ListItemText
+                          primary={move}
+                          />
+                        </ListItem>
+                        <Divider />
+                      </div>
+                    ))}
+                </List>
+              </div> 
+            </Grid>
+            <Grid item xs={3}>
+              <Typography variant="h5" className={classes.title} gutterBottom>
+                Accuracy
+              </Typography>
+            </Grid>
+            <Grid item xs={3}>
+              <Typography variant="h5" className={classes.title} gutterBottom>
+                {accuracy.toFixed(0)}%
+              </Typography>
+            </Grid>
+            <Grid item xs={3}>
+              <Typography variant="h5" className={classes.title} gutterBottom>
+                Avg Sync Delay 
+              </Typography>
+            </Grid>
+            <Grid item xs={3}>
+              <Typography variant="h5" className={classes.title} gutterBottom>
+                {syncDelay.toFixed(2)}
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <UserDanceAccuracyBar dancerName={name}/>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+}
+export default OverallStats;
